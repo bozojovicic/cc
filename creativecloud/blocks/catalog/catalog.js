7d46653138d4460ad84fffe6f2a3f1d56e731b3d
Bozo Jovicic <bozo@hitthecode.com>
2024-07-12 10:33:01 +0200

    MWPW-153245 [merch-card] dispatch sort & search event for analytics

--- creativecloud/blocks/catalog/catalog.js
+++ creativecloud/blocks/catalog/catalog.js
@@ -6,6 +6,24 @@ const { loadStyle } = await import(`${miloLibs}/utils/utils.js`);
 // Helps with TBT: MWPW-145127
 loadStyle(`${miloLibs}/blocks/global-navigation/features/profile/dropdown.css`);
 
+function handleCustomAnalyticsEvent(eventName, element) {
+  let daaLhValue = '';
+  let daaLhElement = element.closest('[daa-lh]');
+  while (daaLhElement) {
+    if (daaLhValue) {
+      daaLhValue = `|${daaLhValue}`;
+    }
+    const daaLhAttrValue = daaLhElement.getAttribute('daa-lh');
+    daaLhValue = `${daaLhAttrValue}${daaLhValue}`;
+    daaLhElement = daaLhElement.parentElement.closest('[daa-lh]');
+  }
+  // eslint-disable-next-line no-underscore-dangle
+  window._satellite?.track('event', {
+    xdm: {},
+    data: { web: { webInteraction: { name: `${eventName}|${daaLhValue}` } } },
+  });
+}
+
 /** container block */
 export default async function init(el) {
   el.classList.add('app');
@@ -28,6 +46,13 @@ export default async function init(el) {
       sidenav.setAttribute('daa-lh', 'b1|sidenav');
       await sidenav.updateComplete;
       if (merchCards) {
+        merchCards.addEventListener('sort-changed', ({ detail }) => {
+          handleCustomAnalyticsEvent(`${detail.value}--sort`, merchCards);
+        });
+        sidenav.search.addEventListener('search-changed', ({ detail }) => {
+          handleCustomAnalyticsEvent(`${detail.value}--search`, sidenav.search);
+        });
+
         sidenav.filters.addEventListener('click', ({ target }) => {
           merchCards.setAttribute('daa-lh', target.getAttribute('daa-ll'));
         });
